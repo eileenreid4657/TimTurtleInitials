51cd9fe06c6957f5932cc264a3b73b7e5e51e12f
Eileen Reid <eileenreid4657@gmail.com>
2020-01-20 10:58:06 -0500

    Cute turtles making my initials

---
+++ index.py
@@ -0,0 +1,46 @@
+import turtle
+
+tim = turtle.Turtle()
+tim.color('red')
+tim.pensize(5)
+tim.shape('turtle')
+tim.speed(5)
+
+#Movement
+tim.forward(100)
+tim.backward(100)
+tim.left(90)
+tim.forward(100)
+tim.right(90)
+tim.forward(75)
+tim.backward(75)
+tim.left(90)
+tim.forward(100)
+tim.right(90)
+tim.forward(100)
+
+dave = turtle.Turtle()
+dave.color('blue')
+dave.pensize(5)
+dave.shape('turtle')
+
+dave.penup()
+dave.forward(110)
+dave.left(90)
+dave.pendown()
+dave.forward(200)
+dave.right(90)
+dave.forward(75)
+dave.right(90)
+dave.forward(75)
+dave.right(90)
+dave.forward(75)
+dave.left(135)
+dave.forward(175)
+
+
+
+
+
+
+delay = input("Press enter to finish")
